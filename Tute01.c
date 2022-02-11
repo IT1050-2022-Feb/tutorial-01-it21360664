4d948b6bb9bab6b2153b2c01a1b99d3af805badd
Ekanayake K.G.V.M it21360664 <99457137+it21360664@users.noreply.github.com>
2022-02-11 14:43:45 +0000

    Done

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,17 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
+int m1,m2;
+float avrg;
+printf("Marks of the subject 1 : ");
+scanf("%d",&m1);
+printf("Marks of the subject 2 : ");
+scanf("%d",&m2);
+avrg=(m1+m2)/2.0;
+
+printf("Average marks %d",avrg);
   
   return 0;
 }
